be88df9c2a17b6ff5b85ba0b22516d1cb1be5f93
Maaz Hassan <maazhassan440@gmail.com>
2023-12-12 13:15:40 +0500

    first react app

--- app1/src/App.jsx
+++ app1/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
+const func = (props) => {
+  let result = props.fName + props.lName;
+  return <h1>name= {result}</h1>;
+};
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <func fName={"Maaz"} lName={"Hassan"} />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
